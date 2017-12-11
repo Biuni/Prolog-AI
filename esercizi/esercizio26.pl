1374e7a1e15a0252d48c9b8616153f8fe32042c0
Gianluca Bonifazi <info.gianlucabonifazi@gmail.com>
2017-12-11 14:18:53 +0100

    Create esercizio26.pl

---
+++ esercizi/esercizio26.pl
@@ -0,0 +1,15 @@
+% ------------------
+% ** ESERCIZIO 26 **
+% ------------------
+% Si considerino i tre simboli del totocalcio 1, x e 2. Sia <partita> un tipo di lista contenente o un
+% simbolo solo (giocata fissa) o due (doppia) o tutt'e tre (tripla). Sia <sistema> una lista di (eventualmente
+% 13) liste del tipo <partita>. Si definisca un predicato sviluppo(<sistema>,MaxX,Max1,Max2)
+% dove MaxX, Max1 e Max2 sono interi positivi, che scriva in output tutte le
+% possibili liste di elementi delle liste di <sistema> (cioé tutte le possibili colonne del sistema) che
+% abbiano un numero di x, di 1 e di 2 rispettivamente minori o uguali a MaxX, Max1 e Max2
+%
+% ?- sviluppo([[1],[2],[1],[x],[1],[1,2],[1,x],[x],[x],[1],[x],[x],[x]],6,7,1).
+% 			   [1,  2,  1,  x,  1,  1,      x,  x,  x,  1,  x,  x,  x]
+% 			   [1,  2,  1,  x,  1,  1,    1,    x,  x,  1,  x,  x,  x]
+
+% Da risolvere...
