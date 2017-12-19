7e31ee87c04fbfc7415b5a54aece65cc8c63e263
Gianluca Bonifazi <info.gianlucabonifazi@gmail.com>
2017-12-19 17:11:10 +0100

    Update esercizio26.pl

--- esercizi/esercizio26.pl
+++ esercizi/esercizio26.pl
@@ -8,8 +8,24 @@
 % possibili liste di elementi delle liste di <sistema> (cioé tutte le possibili colonne del sistema) che
 % abbiano un numero di x, di 1 e di 2 rispettivamente minori o uguali a MaxX, Max1 e Max2
 %
-% ?- sviluppo([[1],[2],[1],[x],[1],[1,2],[1,x],[x],[x],[1],[x],[x],[x]],6,7,1).
-% 			   [1,  2,  1,  x,  1,  1,      x,  x,  x,  1,  x,  x,  x]
-% 			   [1,  2,  1,  x,  1,  1,    1,    x,  x,  1,  x,  x,  x]
+% ?- sviluppo([[1],[2],[1],[x],[1],[1,2],[1,x],[x],[x],[1],[x],[x],[x]],6,6,1).
+% 1,2,1,x,1,1,1,x,x,1,x,x,x]
 
-% Da risolvere...
+sviluppo([], _, _, _).
+sviluppo([T|C], MaxX, Max1, Max2) :-
+	conta(T, MaxX, Max1, Max2, MaxXAgg, Max1Agg, Max2Agg, Res),
+	write(Res), write(','),
+	sviluppo(C, MaxXAgg, Max1Agg, Max2Agg), !.
+
+conta([T|_], MaxX, Max1, Max2, MaxXAgg, Max1, Max2, T) :-
+	T = x,
+	MaxX > 0, MaxXAgg is MaxX - 1.
+conta([T|_], MaxX, Max1, Max2, MaxX, Max1Agg, Max2, T) :-
+	T = 1,
+	Max1 > 0, Max1Agg is Max1 - 1.
+conta([T|_], MaxX, Max1, Max2, MaxX, Max1, Max2Agg, T) :-
+	T = 2,
+	Max2 > 0, Max2Agg is Max2 - 1.
+
+conta([_|C], MaxX, Max1, Max2, MaxXAgg, Max1Agg, Max2Agg, Res) :-
+	conta(C, MaxX, Max1, Max2, MaxXAgg, Max1Agg, Max2Agg, Res).
